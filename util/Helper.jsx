fb8f8f377b9599221ec2341044dce81e0055efac
Adolphus Miracle <miraboy13@gmail.com>
2024-03-14 23:30:09 +0100

    feat: defined helper objects

--- util/Helper.jsx
+++ util/Helper.jsx
@@ -1,1 +1,3 @@
 export const  BatterLevel = (batteryLevel) => Math.round(batteryLevel * 100)
+
+export  const screens = {home: "Home", notification: "Notification", permission: "Permission", optimize: "Optimize" }
