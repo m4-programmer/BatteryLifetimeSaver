5f6ae1644ca21b8bef1de15d577bb8427e1c5abd
Adolphus Miracle <miraboy13@gmail.com>
2024-03-14 19:53:46 +0100

    feat: created component to hold paragraph like text

---
+++ components/P.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const P = ({text}) => {
+  return (
+
+      <Text className="text-base text-gray-300 font-normal">{text}</Text>
+  )
+}
+
+export default P
+
+const styles = StyleSheet.create({})
