c787c5efd056958e475c24d62437e5dc025be02c
Adolphus Miracle <miraboy13@gmail.com>
2024-03-15 07:17:08 +0100

    refactor: refactored home screen

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,16 +1,51 @@
-import { View, Text, ScrollView, StyleSheet, Platform } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, StyleSheet, Platform, Linking } from 'react-native'
+import React, {useEffect} from 'react'
 import Header from '../components/Header'
 import BatteryCard from '../components/BatteryCard';
 import FeatureBlock from '../components/FeatureBlock';
 import { screens } from '../util/Helper';
+import CustomButton from '../components/CustomButton';
+import { Permissions } from 'expo';
+import * as Brightness from 'expo-brightness';
+
 const ios = Platform.OS === 'ios'
 const HomeScreen = (props) => {
     const {isCharging, batteryLevel, isBatteryFull, navigate} = props
+    const [brightness, setBrightness] = React.useState(null);
+
+    const openBatteryUsageSettings = () => {
+      Linking.openSettings();
+    };
+    const reduceBrightness = async () => {
+      try {
+        const { status } = await Brightness.requestPermissionsAsync();
+        // Set the brightness level to a lower value (e.g., 0.5)
+        if (status === 'granted') {
+          Brightness.setSystemBrightnessAsync(0.05);
+        }
+      
+      } catch (error) {
+        console.error('Error setting brightness:', error);
+      }
+    };
+    const saveBattery = async () => {
+      try {
+        const { status } = await Brightness.requestPermissionsAsync();
+        // Set the brightness level to a lower value (e.g., 0.5)
+        if (status === 'granted') {
+          Brightness.setSystemBrightnessAsync(0.0);
+        }
+        alert("Battery Optimized Successfully");
+      } catch (error) {
+        console.error('Error setting brightness:', error);
+      }
+    };
 
   return (
     <>
-        <Header />
+      
+          <Header />
+      
 
         <ScrollView contentContainerStyle={styles.body}>
         {/* Card Section for batter */}
@@ -18,16 +53,16 @@ const HomeScreen = (props) => {
 
         <Text className="text-2xl pt-10 text-white font-extrabold">Features</Text>
         {/* Optimize Brightness */}
-            <FeatureBlock title={"Optimize for battery"} text={"Reduce brightness"} navigate={()=> navigate(screens.optimize)} />
+            <FeatureBlock title={"Optimize for battery"} text={"Reduce brightness"} navigate={reduceBrightness} />
         {/* Set Notification */}
             <FeatureBlock title={"Notification Settings"} text={"Set notification to help you save battery"} navigate={()=>navigate(screens.notification)} />
         {/* Set Permission */}
-        <FeatureBlock title={"Permission Settings"} text={"Set permission"} navigate={()=>navigate(screens.permission)} />
-
-        {/* Top Battery Consuming Apps */}
-        <Text className="text-2xl pt-10 text-white font-extrabold">Top battery-consuming apps</Text>
-        <View >
-
+          <FeatureBlock title={"Apps Battery Consumption"} text={"See apps that are consuming battery"} navigate={()=>openBatteryUsageSettings()} />
+        {/* Using Deep Linking, Link user to their phone setting to see app that is consuming more battery */}
+        
+        {/* Button */}
+        <View className="mt-20">
+          <CustomButton text={"Save Battery"} color={"#fff"} onPress={saveBattery} />
         </View>
         </ScrollView>
     </>
