45a9cb1b68f4de7e5e75dd2d6c25b1267102b0b7
Adolphus Miracle <miraboy13@gmail.com>
2024-03-15 08:35:50 +0100

    feat: implemented v1 of app

--- App.js
+++ App.js
@@ -4,16 +4,19 @@ import React, { useEffect } from 'react'
 import * as Battery from 'expo-battery';
 import HomeScreen from './screens/HomeScreen';
 import NotificationScreen from './screens/NotificationScreen';
-import PermissionScreen from './screens/PermissionScreen';
-import OptimizeScreen from './screens/OptimizeScreen';
-import { screens } from './util/Helper';
+import { BatterLevel, screens } from './util/Helper';
 
 export default function App() {
     const [batteryLevel, setBatteryLevel] = React.useState(null);
     const [isCharging, setIsCharging] = React.useState("No");
     const [isBatteryFull, setIsBatteryFull] = React.useState("No");
     const [navigation, setNavigation] = React.useState("Home");
-   
+   const [notification, setNotification] = React.useState(false);
+   const [toggleValue, setToggleValue] = React.useState(false);
+
+  const handleToggleChange = () => {
+    setToggleValue(!toggleValue);
+  };
     
     useEffect(()=>{
       const getBatteryDetails = async () => {
@@ -23,9 +26,14 @@ export default function App() {
         setIsCharging(batteryState === 2 ? "Yes" : "No");
         setIsBatteryFull(batteryState === 3 ? "Yes" : "No");
         setBatteryLevel(level); // Convert to percentage
+        //alert battery 
+        if (toggleValue && (level) < 0.15) {
+          alert("Battery low");
+        }
     };
         getBatteryDetails()
-
+        // console.log(BatterLevel(batteryLevel));
+        
          // Event listener for battery state changes
          const subscription = Battery.addBatteryStateListener(getBatteryDetails);
 
@@ -36,12 +44,11 @@ export default function App() {
     },[1000])
         
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.container(navigation == screens.home)}>
       
       {navigation == screens.home && <HomeScreen batteryLevel={batteryLevel} isCharging={isCharging} isBatteryFull={isBatteryFull} navigate={setNavigation} />}
-      {navigation == screens.notification && <NotificationScreen navigate={setNavigation} />}
-      {navigation == screens.permission && <PermissionScreen navigate={setNavigation} />}
-      {navigation == screens.optimize && <OptimizeScreen navigate={setNavigation} />}
+      {navigation == screens.notification && <NotificationScreen navigate={setNavigation} toggleValue={toggleValue} handleToggleChange={handleToggleChange} />}
+      
       <StatusBar style="light" />
     </SafeAreaView>
   );
@@ -49,9 +56,11 @@ export default function App() {
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#121717',
-    paddingTop: 100
+  container (isHome)  {
+    return {
+      flex: 1,
+      backgroundColor: '#121717',
+      paddingTop: isHome ? 100 : 50
+    }
   },
 });
