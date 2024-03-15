93d045585e7ab6d30afc341eecec018a442fa1e0
Adolphus Miracle <miraboy13@gmail.com>
2024-03-15 05:43:57 +0100

    feat: implemented logic to chck battery consumption by opening settings page

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, ScrollView, Platform, TouchableOpacity } from 'react-native';
-import Header from './components/Header';
-import BatteryCard from './components/BatteryCard';
+import { StyleSheet, Text, View, SafeAreaView, } from 'react-native';
 import React, { useEffect } from 'react'
 import * as Battery from 'expo-battery';
-
-import FeatureBlock from './components/FeatureBlock';
-const ios = Platform.OS === 'ios'
+import HomeScreen from './screens/HomeScreen';
+import NotificationScreen from './screens/NotificationScreen';
+import PermissionScreen from './screens/PermissionScreen';
+import OptimizeScreen from './screens/OptimizeScreen';
+import { screens } from './util/Helper';
 
 export default function App() {
     const [batteryLevel, setBatteryLevel] = React.useState(null);
     const [isCharging, setIsCharging] = React.useState("No");
     const [isBatteryFull, setIsBatteryFull] = React.useState("No");
-
-    const getBatteryDetails = async () => {
+    const [navigation, setNavigation] = React.useState("Home");
+   
+    
+    useEffect(()=>{
+      const getBatteryDetails = async () => {
         const level = await Battery.getBatteryLevelAsync();
         const mode = await Battery.getPowerStateAsync();
         const batteryState = await Battery.getBatteryStateAsync();
-        console.log(batteryState);
         setIsCharging(batteryState === 2 ? "Yes" : "No");
         setIsBatteryFull(batteryState === 3 ? "Yes" : "No");
         setBatteryLevel(level); // Convert to percentage
     };
-    useEffect(()=>{
         getBatteryDetails()
 
          // Event listener for battery state changes
@@ -33,29 +34,14 @@ export default function App() {
              subscription.remove();
          };
     },[1000])
-    const navigate = () => {}
+        
   return (
     <SafeAreaView style={styles.container}>
       
-      <Header />
-
-      <ScrollView contentContainerStyle={styles.body}>
-        {/* Card Section for batter */}
-        <BatteryCard batteryLevel={batteryLevel} isCharging={isCharging} isBatteryFull={isBatteryFull}/>
-
-        <Text className="text-2xl pt-10 text-white font-extrabold">Features</Text>
-        {/* Optimize Brightness */}
-          <FeatureBlock title={"Optimize for battery"} text={"Reduce brightness"} navigate={navigate} />
-        {/* Set Notification */}
-          <FeatureBlock title={"Notification Settings"} text={"Set notification to help you save battery"} navigate={navigate} />
-        {/* Set Permission */}
-        <FeatureBlock title={"Permission Settings"} text={"Set permission"} navigate={navigate} />
-        {/* Top Battery Consuming Apps */}
-        <View >
-        
-        </View>
-      </ScrollView>
-      
+      {navigation == screens.home && <HomeScreen batteryLevel={batteryLevel} isCharging={isCharging} isBatteryFull={isBatteryFull} navigate={setNavigation} />}
+      {navigation == screens.notification && <NotificationScreen navigate={setNavigation} />}
+      {navigation == screens.permission && <PermissionScreen navigate={setNavigation} />}
+      {navigation == screens.optimize && <OptimizeScreen navigate={setNavigation} />}
       <StatusBar style="light" />
     </SafeAreaView>
   );
@@ -68,8 +54,4 @@ const styles = StyleSheet.create({
     backgroundColor: '#121717',
     paddingTop: 100
   },
-  body: {
-    paddingTop: ios ? 50 : 0,
-    paddingHorizontal: 30,
-  }
 });
